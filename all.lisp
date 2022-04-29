7c99c7bf3d2bfff827a2cc95445b3a32e8e928c1
Olaf Ritter von Ruppert <oruppert@googlemail.com>
2022-04-29 19:29:41 +0200

    remove all.lisp

--- all.lisp
+++
@@ -1,9 +0,0 @@
-(uiop:define-package :webapp/all
-  (:nicknames :webapp)
-  (:use-reexport :webapp/model
-		 :webapp/database
-		 :webapp/html
-		 :webapp/render
-		 :webapp/callbacks
-		 :webapp/form
-		 :webapp/search))
