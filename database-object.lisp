49412dd19502fab42a1011a70d1e8567e9cef782
Olaf Ritter von Ruppert <oruppert@googlemail.com>
2022-06-01 20:39:51 +0200

    database-object.lisp: implement generic functions

--- database-object.lisp
+++ database-object.lisp
@@ -18,3 +18,62 @@
 
 (defgeneric database-delete (object)
   (:documentation "Delete the given object from the database."))
+
+;;;; database-object
+
+(defclass database-object ()
+  ((id :initarg :id :initform nil :accessor object-id)))
+
+(defmethod slot-unbound (class (self database-object) slot-name)
+  (assert (object-id self))
+  (setf (slot-value self slot-name)
+	(caar (postmodern:query
+	       (format nil "select ~a from ~a where id = ~a"
+		       (postmodern:sql-escape slot-name)
+		       (postmodern:sql-escape (class-name class))
+		       (postmodern:sql-escape (object-id self)))))))
+
+
+(defun direct-slots (object)
+  (mapcar #'sb-mop:slot-definition-name
+	  (sb-mop:class-direct-slots (class-of object))))
+
+(defun bound-slots (object)
+  (remove-if-not (lambda (slot-name) (slot-boundp object slot-name))
+		 (direct-slots object)))
+
+(defun bound-values (object)
+  (mapcar (lambda (slot-name) (slot-value object slot-name))
+	  (bound-slots object)))
+
+(defmethod database-insert ((self database-object))
+  (assert (null (object-id self)))
+  (setf (object-id self)
+	(caar
+	 (postmodern:query
+	  (format nil "insert into ~a(~{~a~^, ~}) values(~{~a~^, ~}) returning id"
+		  (postmodern:sql-escape (class-name (class-of self)))
+		  (mapcar #'postmodern:sql-escape (bound-slots self))
+		  (mapcar #'postmodern:sql-escape (bound-values self))))))
+  self)
+
+(defmethod database-update ((self database-object))
+  (assert (object-id self))
+  (postmodern:query
+   (format nil "update ~a set ~{~a = ~a~^, ~} where id = ~a"
+	   (postmodern:sql-escape (class-name (class-of self)))
+	   (mapcan #'list
+		   (mapcar #'postmodern:sql-escape (bound-slots self))
+		   (mapcar #'postmodern:sql-escape (bound-values self)))
+	   (postmodern:sql-escape (object-id self))))
+  self)
+
+(defmethod database-delete ((self database-object))
+  (postmodern:query
+   (format nil "delete from ~a where id = ~a"
+	   (postmodern:sql-escape (class-name (class-of self)))
+	   (postmodern:sql-escape (object-id self))))
+  (setf (object-id self) nil)
+  self)
+
+
