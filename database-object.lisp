fedf1f51519ef11f3fd240984afd26e3edd05843
Olaf Ritter von Ruppert <oruppert@googlemail.com>
2022-06-01 20:50:53 +0200

    database-object.lisp: use utilities package.

--- database-object.lisp
+++ database-object.lisp
@@ -1,7 +1,8 @@
 ;;;; package
 
 (uiop:define-package :webapp/database-object
-  (:use :common-lisp)
+  (:use :common-lisp
+	:webapp/utilities)
   (:export :database-insert
 	   :database-update
 	   :database-delete))
@@ -32,19 +33,6 @@
 		       (postmodern:sql-escape slot-name)
 		       (postmodern:sql-escape (class-name class))
 		       (postmodern:sql-escape (object-id self)))))))
-
-
-(defun direct-slots (object)
-  (mapcar #'sb-mop:slot-definition-name
-	  (sb-mop:class-direct-slots (class-of object))))
-
-(defun bound-slots (object)
-  (remove-if-not (lambda (slot-name) (slot-boundp object slot-name))
-		 (direct-slots object)))
-
-(defun bound-values (object)
-  (mapcar (lambda (slot-name) (slot-value object slot-name))
-	  (bound-slots object)))
 
 (defmethod database-insert ((self database-object))
   (assert (null (object-id self)))
