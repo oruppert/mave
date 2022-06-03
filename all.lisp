85a0586ff443e3f59982cd2be78c22e11393b101
Olaf Ritter von Ruppert <oruppert@googlemail.com>
2022-06-03 14:32:57 +0200

    sub package documentation

--- all.lisp
+++ all.lisp
@@ -1,20 +1,26 @@
-;;;; Package Structure (the webapp/all package)
+;;;; Package Structure
 
 ;;; We are using the ``modern'' lisp package style.  That means every
-;;; file declares its own package and the ``all'' includes all these
-;;; packages.
+;;; file declares its own package and the ``all''-package includes all
+;;; these packages.
 
 ;;; A nice thing about this structure is, that you do not need to
-;;; include the whole ``webapp'' package, but instead reference only
-;;; the packages you actually need.
+;;; include the whole ``webapp'' package, but instead use only the
+;;; packages you actually need.
 
 (uiop:define-package :webapp/all
-  (:use-reexport :webapp/generics
-		 :webapp/html
-		 :webapp/href
-		 :webapp/field
-		 :webapp/render
-		 :webapp/database-object
-		 :webapp/standard-page
-		 :webapp/standard-form)
+  (:use-reexport
+   ;; defines the render generic function
+   :webapp/render
+   ;; a function based html generator
+   :webapp/html
+   ;; a simple data access object
+   :webapp/database-object
+   :webapp/generics
+   ;; constructing uris
+   :webapp/href
+   :webapp/field
+   ;; defines a bare bones web page
+   :webapp/standard-page
+   :webapp/standard-form)
   (:nicknames :webapp))
