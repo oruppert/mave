25bade5f76f1294bc91d8cd5460e63caeac3d313
Olaf Ritter von Ruppert <oruppert@googlemail.com>
2022-06-03 15:35:27 +0200

    continue to try to get links working.

--- introduction.lisp
+++ introduction.lisp
@@ -3,6 +3,6 @@
 ;;; This package contains a bunch of classes that make writing lisp
 ;;; web applications easier.
 
-;;; - [[* Package Structure]]
+;;; - [[Package Structure][#package-structure]]
 
 
