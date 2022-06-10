c67ccce88f1f025580a79aa8fa26ff6ab06a5f18
Olaf Ritter von Ruppert <oruppert@googlemail.com>
2022-06-10 17:05:28 +0200

    standard-page: remove page-title function, use disply-name instead

--- standard-page.lisp
+++ standard-page.lisp
@@ -1,20 +1,14 @@
-;;;; package
+;;;; Standard Page
 
 (uiop:define-package :webapp/standard-page
   (:use :common-lisp
 	:webapp/display-protocol
 	:webapp/html)
-  (:export :page-title
-	   :page-style
+  (:export :page-style
 	   :page-script
 	   :standard-page))
 
 (in-package :webapp/standard-page)
-
-;;;; generic functions
-
-(defgeneric page-title (object page)
-  (:documentation "Returns the title of the given object and page."))
 
 (defgeneric page-style (page)
   (:documentation "Returns the styles of the given page as a list of strings.")
@@ -24,16 +18,8 @@
   (:documentation "Returns the script of the given page as a list of strings.")
   (:method-combination append :most-specific-last))
 
-;;;; standard-page
-
 (defclass standard-page ()
-  ((title :initarg :title)))
-
-(defmethod page-title (object (self standard-page))
-  (if (slot-boundp self 'title)
-      (slot-value self 'title)
-      (when (next-method-p)
-	(call-next-method))))
+  ((name :initarg :name)))
 
 (defmethod page-style append ((self standard-page))
   nil)
@@ -41,14 +27,21 @@
 (defmethod page-script append ((self standard-page))
   nil)
 
+(defmethod display-name (object (self standard-page))
+  (if (slot-boundp self 'name)
+      (slot-value self 'name)
+      (when (next-method-p)
+	(call-next-method))))
+
 (defmethod display (object (self standard-page))
   (print-html-to-string
    (html
     (head
-     (title (page-title object self))
+     (title (display-name object self))
      (meta :name "viewport" :content "width=device-width, initial-scale=1")
      (style (page-style self)))
     (body (when (next-method-p)
 	    (call-next-method))
 	  (script (page-script self))))))
+
 
