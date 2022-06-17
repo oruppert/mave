2f9378eebabad5c76223d9331fc6c565a50bf6a8
Olaf Ritter von Ruppert <oruppert@googlemail.com>
2022-06-18 00:30:51 +0200

    add search-table/database-column.lisp

---
+++ search-table/database-column.lisp
@@ -0,0 +1,57 @@
+;;;; Database Column
+
+(uiop:define-package :webapp/search-table/database-column
+  (:use :common-lisp)
+  (:export
+   #:column-name
+   #:column-qualifier
+   #:column-qualified-name
+   #:column-alias
+   #:column-alias-keyword))
+
+(in-package :webapp/search-table/database-column)
+
+(defgeneric column-name (column)
+  (:documentation "column -> string"))
+
+(defgeneric column-qualifier (column)
+  (:documentation "column -> string"))
+
+(defgeneric column-qualified-name (column)
+  (:documentation "column -> string"))
+
+(defgeneric column-alias (column)
+  (:documentation "column -> string"))
+
+(defgeneric column-alias-keyword (column)
+  (:documentation "column -> symbol"))
+
+(defclass database-column ()
+  ((name :initarg :name :reader column-name)
+   (qualifier :initarg :qualifier :reader column-qualifier)))
+
+(defmethod column-name :around ((self database-column))
+  (let ((value (call-next-method)))
+    (etypecase value
+      (string value)
+      (symbol (postmodern:sql-escape value)))))
+
+(defmethod column-qualifier :around ((self database-column))
+  (let ((value (call-next-method)))
+    (etypecase value
+      (string value)
+      (symbol (postmodern:sql-escape value)))))
+
+(defmethod column-qualified-name ((self database-column))
+  (format nil "~a.~a"
+	  (column-qualifier self)
+	  (column-name self)))
+
+(defmethod column-alias ((self database-column))
+  (format nil "~a_~a"
+	  (column-qualifier self)
+	  (column-name self)))
+
+(defmethod column-alias-keyword ((self database-column))
+  (intern (substitute #\- #\_ (string-upcase (column-alias self)))
+	  :keyword))
