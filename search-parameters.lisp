ac78b19657cc836745995b9670455564048d2c7b
Olaf Ritter von Ruppert <oruppert@googlemail.com>
2022-06-17 18:51:42 +0200

    Add search-parameters.lisp

---
+++ search-parameters.lisp
@@ -0,0 +1,22 @@
+(uiop:define-package :webapp/search-parameters
+    (:use :common-lisp)
+  (:export :search-parameters
+	   :search-columns
+	   :search-offset
+	   :search-limit))
+
+(in-package :webapp/search-parameters)
+
+(defclass search-parameters ()
+  ((columns :initarg :columns :reader search-columns)
+   (offset :initarg :offset :reader search-offset)
+   (limit :initarg :limit :reader search-limit)))
+
+(hunchentoot:define-easy-handler search-parameters
+    ((columns :parameter-type '(keyword) :real-name "column")
+     (offset :parameter-type 'integer :init-form 0)
+     (limit :parameter-type 'integer :init-form 50))
+  (make-instance 'search-parameters
+		 :columns columns
+		 :offset offset
+		 :limit limit))
