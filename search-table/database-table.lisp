e7b7d22a21d3c12811ef1a2000286ceac19286ff
Olaf Ritter von Ruppert <oruppert@googlemail.com>
2022-06-18 11:57:29 +0200

    remove database-table schema

--- search-table/database-table.lisp
+++ search-table/database-table.lisp
@@ -3,7 +3,6 @@
 	:webapp/search-table/database-column
 	:webapp/search-table/database-utilities)
   (:export :table-name
-	   :table-schema
 	   :table-alias
 	   :list-columns
 	   :database-table))
@@ -11,9 +10,6 @@
 (in-package :webapp/search-table/database-table)
 
 (defgeneric table-name (table)
-  (:documentation "table -> string"))
-
-(defgeneric table-schema (table)
   (:documentation "table -> string"))
 
 (defgeneric table-alias (table)
@@ -24,15 +20,11 @@
 
 (defclass database-table ()
   ((name :initarg :name :reader table-name)
-   (schema :initarg :schema :reader table-schema)
    (alias :initarg :alias :reader table-alias)
    (column-names :reader column-names)))
 
-(defun database-table (name &optional (alias name) (schema "public"))
-  (make-instance 'database-table
-		 :name name
-		 :alias alias
-		 :schema schema))
+(defun database-table (name &optional (alias name))
+  (make-instance 'database-table :name name :alias alias))
 
 (defmethod table-name :around ((self database-table))
   (identifier-name (call-next-method)))
@@ -47,8 +39,7 @@
 			 (self database-table)
 			 (slot-name (eql 'column-names)))
   (setf (slot-value self 'column-names)
-	(database-columns (table-name self)
-			  (table-schema self))))
+	(database-columns (table-name self))))
 
 (defmethod list-columns ((self database-table))
   (mapcar (alexandria:curry #'database-column (table-alias self))
