e7673e8aed24f4f882ee3f7d7f2ff1e56d0c1bbc
Olaf Ritter von Ruppert <oruppert@googlemail.com>
2022-06-22 10:53:14 +0200

    standard-page: move page methods to generic functions

--- standard-page.lisp
+++ standard-page.lisp
@@ -15,36 +15,23 @@
 
 (defgeneric page-style (page)
   (:documentation "Returns the styles of the given page as a list of strings.")
-  (:method-combination append :most-specific-last))
+  (:method-combination append :most-specific-last)
+  (:method append (page) nil))
 
 (defgeneric page-script (page)
   (:documentation "Returns the script of the given page as a list of strings.")
-  (:method-combination append :most-specific-last))
+  (:method-combination append :most-specific-last)
+  (:method append (page) nil))
 
 (defgeneric additional-head-elements (page)
   (:documentation "Returns a list of additional html head elements.")
-  (:method-combination append :most-specific-last))
+  (:method-combination append :most-specific-last)
+  (:method append (page) nil))
 
 ;;;; Default Implementation
 
 (defclass standard-page ()
-  ((title :initarg :title)
-   (doctype :initarg :doctype)))
-
-(defmethod display-name (object (self standard-page))
-  (if (slot-boundp self 'title)
-      (slot-value self 'title)
-      (when (next-method-p)
-	(call-next-method))))
-
-(defmethod page-style append ((self standard-page))
-  nil)
-
-(defmethod page-script append ((self standard-page))
-  nil)
-
-(defmethod additional-head-elements append ((self standard-page))
-  nil)
+  ((title :initarg :title)))
 
 (defmethod display-name (object (self standard-page))
   (if (slot-boundp self 'title)
@@ -54,18 +41,14 @@
 
 (defmethod display (object (self standard-page))
   (print-html-to-string
-   (list
-    (when (slot-boundp self 'doctype)
-      (html-string
-       (slot-value self 'doctype)))
-    (html
-     (head
-      (title (display-name object self))
-      (meta :name "viewport" :content "width=device-width, initial-scale=1")
-      (style (page-style self))
-      (additional-head-elements self))
-     (body (when (next-method-p)
-	     (call-next-method))
-	   (script (page-script self)))))))
+   (html
+    (head
+     (title (display-name object self))
+     (meta :name "viewport" :content "width=device-width, initial-scale=1")
+     (style (page-style self))
+     (additional-head-elements self))
+    (body (when (next-method-p)
+	    (call-next-method))
+	  (script (page-script self))))))
 
 
