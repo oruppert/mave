fc800267c5cbe84d7d7d0963972d2d2807217e30
Olaf Ritter von Ruppert <oruppert@googlemail.com>
2022-07-01 11:47:39 +0200

    check within-request and default to current location

--- utilities/http-redirect.lisp
+++ utilities/http-redirect.lisp
@@ -4,7 +4,9 @@
 
 (in-package :webapp/utilities/http-redirect)
 
-(defun http-redirect (location)
-  (setf (hunchentoot:header-out :location) location)
-  (setf (hunchentoot:return-code*) 302)
-  (values nil))
+(defun http-redirect (&optional location)
+  (when (hunchentoot:within-request-p)
+    (setf (hunchentoot:header-out :location)
+	  (or location (hunchentoot:request-uri*)))
+    (setf (hunchentoot:return-code*) 302)
+    (values nil)))
