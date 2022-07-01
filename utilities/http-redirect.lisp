b1355e63f9c8bfa2d96c4469e81faaa5c7659731
Olaf Ritter von Ruppert <oruppert@googlemail.com>
2022-07-01 11:54:34 +0200

    documentation

--- utilities/http-redirect.lisp
+++ utilities/http-redirect.lisp
@@ -5,6 +5,10 @@
 (in-package :webapp/utilities/http-redirect)
 
 (defun http-redirect (&optional location)
+  "Works like hunchentoot:redirect, but does not throw.
+Should be the last statement in a define-easy-handler.  Note! Does not
+normalize relative urls like hunchentoot:redirect, but most browsers
+support relative urls."
   (when (hunchentoot:within-request-p)
     (setf (hunchentoot:header-out :location)
 	  (or location (hunchentoot:request-uri*)))
