a8d0fa51cf11277b4c3bd7176478746762b43278
Olaf Ritter von Ruppert <oruppert@googlemail.com>
2022-07-03 01:24:45 +0200

    remove page protocol default implementations

--- standard-page/page-protocol.lisp
+++ standard-page/page-protocol.lisp
@@ -10,25 +10,20 @@
 
 (defgeneric page-script-uris (page)
   (:documentation "Retuns a list of script uris for the given page.")
-  (:method-combination append :most-specific-last)
-  (:method append (page) nil))
+  (:method-combination append :most-specific-last))
 
 (defgeneric page-style-uris (page)
   (:documentation "Returns a list of stylesheet uris for the given page.")
-  (:method-combination append :most-specific-last)
-  (:method append (page) nil))
+  (:method-combination append :most-specific-last))
 
 (defgeneric page-style (page)
   (:documentation "Returns the styles of the given page as a list of strings.")
-  (:method-combination append :most-specific-last)
-  (:method append (page) nil))
+  (:method-combination append :most-specific-last))
 
 (defgeneric page-script (page)
   (:documentation "Returns the script of the given page as a list of strings.")
-  (:method-combination append :most-specific-last)
-  (:method append (page) nil))
+  (:method-combination append :most-specific-last))
 
 (defgeneric additional-head-elements (page)
   (:documentation "Returns a list of additional html head elements.")
-  (:method-combination append :most-specific-last)
-  (:method append (page) nil))
+  (:method-combination append :most-specific-last))
