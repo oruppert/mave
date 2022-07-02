3c00d463c3432548b4c757449e61caaf99ea5103
Olaf Ritter von Ruppert <oruppert@googlemail.com>
2022-07-03 01:38:16 +0200

    rename page-protocol functions

--- standard-page/page-protocol.lisp
+++ standard-page/page-protocol.lisp
@@ -1,29 +1,29 @@
 (uiop:define-package :webapp/standard-page/page-protocol
     (:use :common-lisp)
-  (:export :page-script-uris
-	   :page-style-uris
-	   :page-style
-	   :page-script
-	   :additional-head-elements))
+  (:export :page-external-scripts
+	   :page-external-styles
+	   :page-inline-style
+	   :page-inline-script
+	   :page-additional-head-elements))
 
 (in-package :webapp/standard-page/page-protocol)
 
-(defgeneric page-script-uris (page)
+(defgeneric page-external-scripts (page)
   (:documentation "Retuns a list of script uris for the given page.")
   (:method-combination append :most-specific-last))
 
-(defgeneric page-style-uris (page)
+(defgeneric page-external-styles (page)
   (:documentation "Returns a list of stylesheet uris for the given page.")
   (:method-combination append :most-specific-last))
 
-(defgeneric page-style (page)
+(defgeneric page-inline-style (page)
   (:documentation "Returns the styles of the given page as a list of strings.")
   (:method-combination append :most-specific-last))
 
-(defgeneric page-script (page)
-  (:documentation "Returns the script of the given page as a list of strings.")
+(defgeneric page-inline-script (page)
+  (:documentation "Returns the scripts of the given page as a list of strings.")
   (:method-combination append :most-specific-last))
 
-(defgeneric additional-head-elements (page)
+(defgeneric page-additional-head-elements (page)
   (:documentation "Returns a list of additional html head elements.")
   (:method-combination append :most-specific-last))
