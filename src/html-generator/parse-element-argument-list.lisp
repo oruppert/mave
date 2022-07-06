e5c289c5af9bf4ec5ae1812596d8381e625c4305
Olaf Ritter von Ruppert <oruppert@googlemail.com>
2022-07-06 20:40:10 +0200

    use nconc

--- src/html-generator/parse-element-argument-list.lisp
+++ src/html-generator/parse-element-argument-list.lisp
@@ -2,15 +2,15 @@
   (:use :common-lisp)
   (:export :parse-element-argument-list))
 
-(in-package :webapp/html-generator/parse-html-lambda-list)
+(in-package :webapp/html-generator/parse-element-argument-list)
 
 (defun parse-element-argument-list (argument-list)
-  "Returns the attributes and children of an element argument list.
+  "Returns the attributes and children of the given element argument list.
 An element argument list consists of attributes and children.
 Attributes are key-value pairs, all other elements are children.  Note
 that the argument-list gets flattened before parsing.  This allows
 functions to return attributes as two element lists."
-  (loop with argument-list = (alexandria:flatten argument-list)
+  (loop with argument-list = (flatten argument-list)
 	while argument-list
 	for item = (pop argument-list)
 	when (keywordp item) collect item into attributes
@@ -18,6 +18,11 @@ functions to return attributes as two element lists."
 	else collect item into children
 	finally (return (values attributes children))))
 
-
+(defun flatten (list)
+  (loop for item in list
+	when (atom item)
+	  collect item
+	else
+	  nconc (flatten item)))
 
 
