3eed46cd7e75febc65947e7805b23e4e83b1b1df
Olaf Ritter von Ruppert <oruppert@googlemail.com>
2022-07-09 11:58:38 +0200

    print-html: remove list specialization and formatting

--- src/html-generator/print-html.lisp
+++ src/html-generator/print-html.lisp
@@ -1,5 +1,3 @@
-;;;; Print Html
-
 (uiop:define-package :webapp/html-generator/print-html
     (:use :common-lisp)
   (:export :print-html-to-string
@@ -7,37 +5,25 @@
 
 (in-package :webapp/html-generator/print-html)
 
-;;;; Print Html To String
-
 (defun print-html-to-string (&rest objects)
   (with-output-to-string (stream)
-    (print-html objects stream)))
+    (dolist (object objects)
+      (print-html object stream))))
 
-;;;; Generic Function
-
-(defgeneric print-html (object stream))
-
-;;;; Default Methods
-
-(defmethod print-html (object stream)
-  (print-html (princ-to-string object) stream))
-
-(defmethod print-html ((null null) stream))
-
-(defmethod print-html ((object (eql :null)) stream))
-
-(defmethod print-html ((list list) stream)
-  (dolist (item list)
-    (print-html item stream)))
-
-(defmethod print-html ((symbol symbol) stream)
-  (write-string (string-downcase symbol) stream))
-
-(defmethod print-html ((string string) stream)
-  (loop for char across string do
-    (case char
-      (#\< (write-string "&lt;" stream))
-      (#\> (write-string "&gt;" stream))
-      (#\& (write-string "&amp;" stream))
-      (#\" (write-string "&quot;" stream))
-      (t (write-char char stream)))))
+(defgeneric print-html (object stream)
+  (:method (object stream)
+    (print-html (princ-to-string object) stream))
+  (:method ((null null) stream)
+    (values))
+  (:method ((null (eql :null)) stream)
+    (values))
+  (:method ((symbol symbol) stream)
+    (write-string (string-downcase symbol) stream))
+  (:method ((string string) stream)
+    (loop for char across string do
+      (case char
+	(#\< (write-string "&lt;" stream))
+	(#\> (write-string "&gt;" stream))
+	(#\& (write-string "&amp;" stream))
+	(#\" (write-string "&quot;" stream))
+	(t (write-char char stream))))))
