7e2bb0af93dcd1682fb9468aa9cea56a215f8846
Olaf Ritter von Ruppert <oruppert@googlemail.com>
2022-07-19 11:46:19 +0200

    complete form protocol

--- src/forms/form-protocol.lisp
+++ src/forms/form-protocol.lisp
@@ -1,35 +1,45 @@
 (uiop:define-package :mave/forms/form-protocol
-    (:use :common-lisp
-	  :mave/forms/redirect-mixin
-	  :mave/forms/input-protocol
-	  :mave/html/all)
-  (:export
-   #:form-slots
-   #:form-update
-   #:form-render))
+    (:use :common-lisp)
+  (:export))
 
 (in-package :mave/forms/form-protocol)
 
-(defgeneric form-slots (object)
-  (:method (object)
-    (mapcar #'closer-mop:slot-definition-name
-	    (closer-mop:class-slots (class-of object)))))
+(defgeneric form-slots (form object))
 
-(defgeneric form-update (object)
-  (:documentation "Updates the given object from http post values.")
-  (:method (object)
-    (loop for slot-name in (form-slots object)
-	  for parameter-name = (input-name object slot-name)
-	  for parameter-value = (hunchentoot:post-parameter parameter-name)
-	  do (setf (input-value object slot-name)
-		   parameter-value))))
+(defgeneric form-update-model (form object))
 
-(defgeneric form-render (object &key slot-reader)
-  (:method (object &key slot-reader)
-    (loop for slot-name in (form-slots object)
-	  for value = (funcall slot-reader object slot-name)
-	  collect (p (label (input-label object slot-name)
-			    (render-input object slot-name value))))))
+(defgeneric form-render-body (form object))
+
+(defgeneric form-input-name (form object slot-name))
+
+(defgeneric form-input-label (form object slot-name))
+
+(defgeneric from-input-value (form object slot-name))
+
+(defgeneric (setf form-input-value) (value form object slot-name))
+
+(defgeneric form-render-input (form object slot-name value))
+
+(defclass insert-form (standard-form) ())
+
+(defmethod handle :after ((self insert-form) object (method (eql :post)))
+  #+nil
+  (database-insert object))
+
+(defmethod form-input-value ((self insert-form) object slot-name)
+  nil)
+
+(defclass update-form (standard-form) ())
+
+(defmethod form-input-value ((self update-form) object slot-name)
+  (values nil))
+
+(defmethod handle :after ((self update-form) object (method (eql :post)))
+  #+nil
+  (database-update object))
+
+
+
 
 
 
