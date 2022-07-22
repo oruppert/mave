bf0b01f5c540c99f7ead58fa3fcb26b3d2784e00
Olaf Ritter von Ruppert <oruppert@googlemail.com>
2022-07-22 12:16:28 +0200

    confirmation-form.lisp: remove merge view and controler into form

--- src/forms/confirmation-form.lisp
+++ src/forms/confirmation-form.lisp
@@ -5,15 +5,13 @@
 	:mave/standard-page/page-protocol
 	:mave/forms/redirect-mixin)
   (:export :confirmed-p
-	   :confirmation-controller
-	   :confirmation-view
 	   :confirmation-form))
 
 (in-package :mave/forms/confirmation-form)
 
 (defgeneric confirmed-p (object))
 
-(defclass confirmation-controller (redirect-mixin)
+(defclass confirmation-form (redirect-mixin)
   ((yes-location :initarg :yes-location :reader yes-location)
    (no-location :initarg :no-location :reader no-location)))
 
@@ -22,26 +20,22 @@
       (yes-location self)
       (no-location self)))
 
-(defclass confirmation-view () ())
-
-(defmethod confirmed-p ((self confirmation-view))
+(defmethod confirmed-p ((self confirmation-form))
   (equal "yes" (hunchentoot:parameter "answer")))
 
-(defmethod display (object (self confirmation-view))
+(defmethod display (object (self confirmation-form))
   (form :method :post
 	:class :confirmation
 	(p :class :message)
 	(p (button :class :yes :name "answer" :value "yes")
 	   (button :class :no :name "answer" :value "no"))))
 
-(defmethod page-inline-styles append ((self confirmation-view))
-  (list #.(uiop:read-file-string
-	   (make-pathname :type "css"
-			  :defaults *compile-file-truename*))))
+(defmethod page-inline-styles append ((self confirmation-form))
+  '(#.(uiop:read-file-string
+       (make-pathname :type "css"
+		      :defaults *compile-file-truename*))))
 
-(defclass confirmation-form (confirmation-controller
-			     confirmation-view)
-  ())
+
 
 
 
